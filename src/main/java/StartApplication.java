1273544f6cdf46b3c785fd52123500cd23e502d5
Griga21 <griga.k.r21@gmail.com>
2024-12-09 16:12:08 +0300

    add new elements

--- src/main/java/StartApplication.java
+++ src/main/java/StartApplication.java
@@ -1,8 +1,4 @@
-import org.gui.MainFrame;
-
 public class StartApplication {
     public static void main(String[] args) {
-        MainFrame mainFrame = new MainFrame();
-        mainFrame.initFrame("src/main/resources/application.properties");
     }
 }
