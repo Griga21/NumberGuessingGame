8846a1fa8ca094b6400e28c87021a87e803bf53c
Griga21 <griga.k.r21@gmail.com>
2024-12-09 17:39:45 +0300

    add game logic

--- src/main/java/org/logic/GameLogic.java
+++ src/main/java/org/logic/GameLogic.java
@@ -1,5 +1,27 @@
 package org.logic;
 
+import java.util.ArrayList;
+
 public class GameLogic {
+    private int minIndex;
+    private int maxIndex;
+    private int resultIndex;
+    private ArrayList<Integer> arrayList = new ArrayList<>();
+    private int sizeArray = 101;
+
+    public void initArrayList() {
+        for (int i = 0; i < sizeArray; i++) arrayList.add(i);
+        resultIndex = (int) Math.ceil(sizeArray / 2);
+    }
+
+    public Integer guessNumber(boolean less) {
+        if (less)
+            maxIndex = resultIndex;
+        else
+            minIndex = resultIndex;
+
+        resultIndex = (int) Math.ceil((maxIndex - minIndex) / 2);
+        return resultIndex;
+    }
 
 }
