fc967fd9ac08d1c1c2499b2818798a26071954f6
Griga21 <griga.k.r21@gmail.com>
2024-12-10 17:17:02 +0300

    add new static variables

--- src/main/java/org/gui/Utils.java
+++ src/main/java/org/gui/Utils.java
@@ -7,4 +7,5 @@ public class Utils {
     public static GamePanel gamePanel;
     public static MainFrame frame;
     public static QuestionPanel questionPanel;
+    public static GameLogic gameLogic;
 }
